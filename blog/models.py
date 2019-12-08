f9b2e69844f2473f6a5ef23f2666300501bdd184
jhyoon <jh3859025@gmail.com>
2019-12-09 00:00:42 +0900

    djangogirls

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+
 #from 또는 import로 시작하는 부분은 다른 파일에 있는 것을 추가하라는 뜻 입니다.
 
 
@@ -13,12 +14,12 @@ class Post (models.Model):
     published_date = models.DateTimeField(
             blank=True, null=True)
 
-def publish(self):
-    self.published_date = timezone.now()
-    self.save()
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
 
-def __str__(self):
-    return self.title
+    def __str__(self):
+        return self.title
 
 
 
